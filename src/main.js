6935049fe2c29680019e30b0a3036e236753359d
progweb-m <614537575@qq.com>
2023-09-18 15:44:04 +0800

    :sparkles: improve options

--- src/main.js
+++ src/main.js
@@ -6,8 +6,13 @@ import router from "./router/index";
 // element
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
 app.use(router);
 app.use(ElementPlus);
 
--- src/views/questions/Questions.vue
+++ src/views/questions/Questions.vue
@@ -2,15 +2,8 @@
   <div>
     <el-button @click="addQuestions">添加题目</el-button>
 
-    <el-drawer
-      title="我是标题"
-      v-model="drawerVisible"
-      direction="rtl"
-      size="50%"
-      :destroy-on-close="true"
-      :show-close="true"
-      :wrapperClosable="false"
-    >
+    <el-drawer title="我是标题" v-model="drawerVisible" direction="rtl" size="50%" :destroy-on-close="true" :show-close="true"
+      :wrapperClosable="false">
       <div>
         <el-form :model="questions" ref="ruleFormRef">
           <el-form-item label="标题" prop="title">
@@ -20,20 +13,36 @@
           <el-form-item label="内容" prop="content">
             <div>
               <div style="border: 1px solid #ccc">
-                <Toolbar
-                  style="border-bottom: 1px solid #ccc"
-                  :editor="editorRef"
-                  :defaultConfig="toolbarConfig"
-                  :mode="mode"
-                />
-                <Editor
-                  style="height: 400px; overflow-y: hidden"
-                  v-model="valueHtml"
-                  :defaultConfig="editorConfig"
-                  :mode="mode"
-                  @onCreated="handleCreated"
-                  @onChange="handleChange"
-                />
+                <Toolbar style="border-bottom: 1px solid #ccc" :editor="editorRef" :defaultConfig="toolbarConfig"
+                  :mode="mode" />
+                <Editor style="height: 400px; overflow-y: hidden" v-model="valueHtml" :defaultConfig="editorConfig"
+                  :mode="mode" @onCreated="handleCreated" @onChange="handleChange" />
+              </div>
+            </div>
+          </el-form-item>
+
+
+          <!-- 选项 -->
+          <el-form-item label="选项" prop="options">
+            <div class="w-full">
+              <el-radio-group v-model="optionsType">
+                <el-radio :label="1" size="large">选择题</el-radio>
+                <el-radio :label="2" size="large">填空</el-radio>
+                <el-radio :label="3" size="large">大题</el-radio>
+              </el-radio-group>
+              <!-- 选择题,四个选项 -->
+              <div v-for="i in 4" class="flex w-full mb-1" >
+                <div class="w-16 ml-1">{{ `选项：`+i }}</div>
+                <el-input  v-model="textarea2" :autosize="autosize" type="textarea"
+                  placeholder="Please input" />
+              </div>
+
+
+
+              <div class="addOption">
+                <el-icon :size="30" style="border: 1px solid #000;cursor: pointer;">
+                  <Plus />
+                </el-icon>
               </div>
             </div>
           </el-form-item>
@@ -64,6 +73,10 @@ const editorConfig = {
     },
   },
 };
+
+const optionsType = ref(1)
+
+const autosize = { minRows: 2, maxRows: 4 }
 
 // 内容 HTML
 const valueHtml = ref("");
